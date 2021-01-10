093769a8d2733c0e35213112d6676dde6b3a2f9c
BlueManCZ <ivo97@centrum.cz>
2021-01-10 11:38:43 +0100

    Add basic UI settings

---
+++ spotify/src/configwidget.cpp
@@ -0,0 +1,15 @@
+// Copyright (C) 2014-2018 Manuel Schneider
+
+#include "configwidget.h"
+
+/** ***************************************************************************/
+Spotify::ConfigWidget::ConfigWidget(QWidget *parent) : QWidget(parent) {
+    ui.setupUi(this);
+}
+
+
+
+/** ***************************************************************************/
+Spotify::ConfigWidget::~ConfigWidget() {
+
+}
