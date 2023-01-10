458987f48b194214462262a170debb8070a86d2f
Manuel Schneider <manuelschneid3r@gmail.com>
2023-01-11 00:49:16 +0100

    Handcraft tld validation. Make handler global.
    Qt dropped it.
    
    Fix https://github.com/albertlauncher/plugins/issues/80

--- urlhandler/src/plugin.cpp
+++ urlhandler/src/plugin.cpp
@@ -1,28 +1,59 @@
 // Copyright (c) 2022 Manuel Schneider
 
-#include <QRegularExpression>
-#include <QDesktopServices>
-#include <QUrl>
 #include "plugin.h"
+#include <QDesktopServices>
+#include <QFile>
+#include <QTextStream>
+#include <QUrl>
 using namespace std;
 using namespace albert;
 
-void ::Plugin::handleQuery(Query &query) const
+Plugin::Plugin()
 {
-    QUrl url = QUrl::fromUserInput(query.string().trimmed());
+    QFile file(":tlds");
+    if (!file.open(QIODevice::ReadOnly))
+        throw runtime_error(QString("Unable to read tld resource").toStdString());
+    valid_tlds << QTextStream(&file).readAll().split("\n");
+    std::sort(valid_tlds.begin(), valid_tlds.end());
+}
+
+vector<RankItem> Plugin::rankItems(const QString &string, const bool& isValid) const
+{
+    vector<RankItem> results;
+    auto trimmed = string.trimmed();
+    auto url = QUrl::fromUserInput(trimmed);
 
     // Check syntax and TLD validity
-    if ( url.isValid() && ( query.string().startsWith("http://") ||  // explict scheme
-                            query.string().startsWith("https://") ||  // explict scheme
-                            ( QRegularExpression(R"R(\S+\.\S+$)R").match(url.host()).hasMatch()))) {// &&
-        //!url.topLevelDomain().isNull()) ) ) {  // valid TLD
+    if (!url.isValid())
+        return results;
 
-        query.add(StandardItem::make(
+    if ((url.scheme() == "http" || url.scheme() == "https")){
+
+        auto tld = url.host().section('.', -1, -1);
+
+        // Skip tld only queries
+        if (trimmed.size() == tld.size())
+            return results;
+
+        // validate top level domain if scheme is not given (http assumed)
+        if (!trimmed.startsWith("http") && !binary_search(valid_tlds.begin(), valid_tlds.end(), tld))
+            return results;
+
+        results.emplace_back(
+            StandardItem::make(
                 "url_hanlder",
                 "Open URL in browser",
                 QString("Visit %1").arg(url.authority()),
                 {"xdg:www", "xdg:web-browser", "xdg:emblem-web", ":default"},
-                {{"open_url","Open URL", [url](){ QDesktopServices::openUrl(url); }}}
-        ));
+                {{
+                    "open_url",
+                    "Open URL",
+                    [url](){
+                        QDesktopServices::openUrl(url);
+                    }
+                }})
+            , MAX_SCORE
+            );
     }
+    return results;
 }
--- urlhandler/src/plugin.h
+++ urlhandler/src/plugin.h
@@ -5,9 +5,12 @@
 
 class Plugin:
         public albert::ExtensionPlugin,
-        public albert::QueryHandler
+        public albert::GlobalQueryHandler
 {
     Q_OBJECT ALBERT_PLUGIN
 public:
-    void handleQuery(Query&) const override;
+    Plugin();
+    std::vector<albert::RankItem> rankItems(const QString &string, const bool& isValid) const override;
+private:
+    QStringList valid_tlds;
 };
