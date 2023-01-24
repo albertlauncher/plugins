690d99ac7998fe4c2dc23d5c8bf5f651a7a1edaf
Manuel Schneider <manuelschneid3r@gmail.com>
2023-01-24 14:50:10 +0100

    [ws] input validation in search engine editor

--- websearch/src/searchengineeditor.cpp
+++ websearch/src/searchengineeditor.cpp
@@ -1,12 +1,13 @@
-// Copyright (C) 2014-2021 Manuel Schneider
+// Copyright (c) 2023 Manuel Schneider
 
 #include <QFileDialog>
 #include <QStandardPaths>
+#include <QMessageBox>
 #include "searchengineeditor.h"
 
 
 SearchEngineEditor::SearchEngineEditor(const SearchEngine &searchEngine, QWidget *parent)
-    : QDialog(parent), searchEngine_(searchEngine)
+        : QDialog(parent), searchEngine_(searchEngine)
 {
     ui.setupUi(this);
     setWindowModality(Qt::WindowModal);
@@ -17,28 +18,40 @@ SearchEngineEditor::SearchEngineEditor(const SearchEngine &searchEngine, QWidget
     ui.toolButton_icon->setIcon(QIcon(searchEngine.iconPath));
 
     connect(ui.lineEdit_name, &QLineEdit::textChanged,
-            [this](const QString & text){ searchEngine_.name = text; });
+            [this](const QString &text) { searchEngine_.name = text; });
 
     connect(ui.lineEdit_trigger, &QLineEdit::textChanged,
-            [this](const QString & text){ searchEngine_.trigger = text; });
+            [this](const QString &text) { searchEngine_.trigger = text; });
 
     connect(ui.lineEdit_url, &QLineEdit::textChanged,
-            [this](const QString & text){ searchEngine_.url = text; });
+            [this](const QString &text) { searchEngine_.url = text; });
 
     connect(ui.toolButton_icon, &QToolButton::clicked,
-            [this](){
+            [this]() {
 
-        QString fileName =
-                QFileDialog::getOpenFileName(
-                    this,
-                    tr("Choose icon"),
-                    QStandardPaths::writableLocation(QStandardPaths::HomeLocation),
-                    tr("Images (*.png *.svg)"));
+                QString fileName =
+                        QFileDialog::getOpenFileName(
+                                this,
+                                tr("Choose icon"),
+                                QStandardPaths::writableLocation(QStandardPaths::HomeLocation),
+                                tr("Images (*.png *.svg)"));
 
-        if(fileName.isEmpty())
-            return;
+                if (fileName.isEmpty())
+                    return;
 
-        searchEngine_.iconPath = fileName;
-        ui.toolButton_icon->setIcon(QIcon(fileName));
+                searchEngine_.iconPath = fileName;
+                ui.toolButton_icon->setIcon(QIcon(fileName));
+            });
+
+    disconnect(ui.buttonBox, &QDialogButtonBox::accepted,
+               this, &QDialog::accept);
+
+    connect(ui.buttonBox, &QDialogButtonBox::accepted, [this]() {
+        if (ui.lineEdit_name->text().trimmed().isEmpty()
+            || ui.lineEdit_trigger->text().trimmed().isEmpty()
+            || ui.lineEdit_url->text().trimmed().isEmpty())
+            QMessageBox::warning(this, "Invalid", "None of the fields must be empty.");
+        else
+            accept();
     });
 }
