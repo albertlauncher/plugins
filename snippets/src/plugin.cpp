e9c39a3c7d33290b426a17f6b32dd94d749cae9b
Manuel Schneider <manuelschneid3r@gmail.com>
2023-06-22 19:15:24 +0200

    [snip:1.3] New features
    * Add "Add" and "Remove" button in config widget
    * Add "Add snippet" item on "add" query
    * Add "Remove" action to snippet items

--- snippets/src/plugin.cpp
+++ snippets/src/plugin.cpp
@@ -1,8 +1,10 @@
-// Copyright (c) 2022 Manuel Schneider
+// Copyright (c) 2023 Manuel Schneider
 
 #include "plugin.h"
 #include "ui_configwidget.h"
 #include <QFileSystemModel>
+#include <QMessageBox>
+#include <QInputDialog>
 #include <QTextStream>
 #include <memory>
 ALBERT_LOGGING
@@ -11,42 +13,47 @@ using namespace albert;
 
 struct SnippetItem : Item
 {
-    SnippetItem(const QFileInfo& fi) : file_name_(fi.fileName()) {}
+    SnippetItem(const QFileInfo& fi, Plugin *p) : file_name_(fi.fileName()), plugin_(p) {}
     QString id() const override { return file_name_; }
     QString text() const override { return file_name_; }
     QString subtext() const override { return QString("Text snippet '%1'").arg(file_name_); }
     QStringList iconUrls() const override { return {":snippet"}; }
     vector<Action> actions() const override {
         return {
-            {"snip-copy-text", "Copy snippet to clipboard", [this](){
-                QFile f(QDir(path).filePath(file_name_));
-                f.open(QIODevice::ReadOnly);
-                setClipboardText(QTextStream(&f).readAll());
-            }},
-            {"snip-open", "Open snippet", [this]() {
-                openUrl("file://" + QDir(path).filePath(file_name_));
-            }}
+            {
+                "copy", "Copy snippet to clipboard",
+                [this](){
+                    QFile f(QDir(plugin_->snippets_path).filePath(file_name_));
+                    f.open(QIODevice::ReadOnly);
+                    setClipboardText(QTextStream(&f).readAll());
+                }
+            },
+            {
+                "open", "Open snippet",
+                [this]() { openUrl("file://" + QDir(plugin_->snippets_path).filePath(file_name_)); }
+            },
+            {
+                "remove", "Remove snippet",
+                [this]() { plugin_->removeSnippet(file_name_); }
+            }
         };
     }
 
     const QString file_name_;
-    static QString path;
+    Plugin * const plugin_;
 };
-QString SnippetItem::path;
 
 
-Plugin::Plugin()
+Plugin::Plugin(): snippets_path(configDir().path())
 {
-    SnippetItem::path = configDir().path();
-
-    fs_watcher.addPath(SnippetItem::path);
+    fs_watcher.addPath(snippets_path);
     connect(&fs_watcher, &QFileSystemWatcher::directoryChanged, this, [this](){updateIndexItems();});
 
     indexer.parallel = [this](const bool &abort){
         vector<IndexItem> r;
         for (const auto &f : configDir().entryInfoList(QDir::Files)){
             if (abort) return r;
-            r.emplace_back(make_shared<SnippetItem>(f), f.fileName());
+            r.emplace_back(make_shared<SnippetItem>(f, this), f.fileName());
         }
         return r;
     };
@@ -55,32 +62,84 @@ Plugin::Plugin()
     };
 }
 
-QString Plugin::defaultTrigger() const
-{
-    return "snip ";
-}
+QString Plugin::defaultTrigger() const { return "snip "; }
+
+QString Plugin::synopsis() const { return "<filter>|[add]"; }
 
 void Plugin::updateIndexItems() { indexer.run(); }
 
+void Plugin::handleTriggerQuery(TriggerQuery *query) const
+{
+    if (query->string() == "add")
+        query->add(
+            StandardItem::make(
+                "snip-add",
+                "Add new snippet",
+                "Create snippet file and open it in default editor.",
+                {":snippet"},
+                {
+                    {"add", "Add snippet", [this](){ newSnippet(nullptr); }}
+                }
+            )
+        );
+    else
+        IndexQueryHandler::handleTriggerQuery(query);
+}
+
+void Plugin::newSnippet(QWidget *parent) const
+{
+    QString text = QInputDialog::getText(parent, qApp->applicationName(),
+                                         "Enter the snippet name");
+    if (!text.isNull()){
+        if (!text.isEmpty()){
+            QFile file(QDir(snippets_path).filePath(text));
+            file.open(QIODevice::WriteOnly);
+            openUrl("file://" + file.fileName());
+        } else
+            QMessageBox::information(parent, qApp->applicationName(),
+                                     "The snippet name must not be empty.");
+    }
+}
+
+void Plugin::removeSnippet(const QString &file_name) const
+{
+    auto path = QDir(snippets_path).filePath(file_name);
+    if (!QFile::exists(path))
+        WARN << "Path to remove does not exist:" << path;
+    else if (QMessageBox::question(nullptr, qApp->applicationName(),
+                                   QString("Move snippet '%1' to trash?").arg(file_name)) == QMessageBox::Yes)
+        if (!QFile::moveToTrash(path))
+            QMessageBox::warning(nullptr, qApp->applicationName(),
+                                 "Failed to move snippet file to trash.");
+}
+
 QWidget *Plugin::buildConfigWidget()
 {
-    auto w = new QWidget;
+    auto *w = new QWidget;
     Ui::ConfigWidget ui;
     ui.setupUi(w);
 
-    auto model = new QFileSystemModel();
+    auto *model = new QFileSystemModel;
     connect(w, &QObject::destroyed, model, &QObject::deleteLater);
-    model->setRootPath(SnippetItem::path);
+    model->setRootPath(snippets_path);
     model->setFilter(QDir::Files);
     model->setReadOnly(false);
     ui.listView->setModel(model);
-    ui.listView->setRootIndex(model->index(SnippetItem::path));
+    ui.listView->setRootIndex(model->index(snippets_path));
 
-    connect(ui.listView, &QListView::activated, this, [model](const QModelIndex &index){
-        openUrl(QString("file://%1").arg(model->filePath(index)));
-    });
-    connect(ui.pushButton_opendir, &QPushButton::clicked, [](){
-        openUrl(QString("file://%1").arg(SnippetItem::path));
+    connect(ui.listView, &QListView::activated, this,
+            [model](const QModelIndex &index){ openUrl(QString("file://%1").arg(model->filePath(index))); });
+
+    connect(ui.pushButton_opendir, &QPushButton::clicked, this,
+            [this](){ openUrl(QString("file://%1").arg(snippets_path)); });
+
+    connect(ui.pushButton_add, &QPushButton::clicked,
+             this, [this, w=w](){ newSnippet(w); });
+
+    connect(ui.pushButton_remove, &QPushButton::clicked, this,
+            [this, model, lw=ui.listView](){
+        if (lw->currentIndex().isValid())
+            removeSnippet(model->filePath(lw->currentIndex()));
     });
 
     return w;
--- snippets/src/plugin.h
+++ snippets/src/plugin.h
@@ -1,9 +1,11 @@
-// Copyright (c) 2022 Manuel Schneider
+// Copyright (c) 2023 Manuel Schneider
 
 #pragma once
+#include <QFileSystemWatcher>
 #include <QObject>
 #include <memory>
 #include "albert.h"
+class QWidget;
 
 class Plugin:
         public albert::ExtensionPlugin,
@@ -12,11 +14,19 @@ class Plugin:
     Q_OBJECT ALBERT_PLUGIN
 public:
     Plugin();
+
+    void newSnippet(QWidget *parent) const;
+    void removeSnippet(const QString &file_name) const;
+
+    const QString snippets_path;
+
+private:
     QString defaultTrigger() const override;
     QWidget* buildConfigWidget() override;
     void updateIndexItems() override;
+    QString synopsis() const override;
+    void handleTriggerQuery(TriggerQuery *) const override;
 
-public:
     QFileSystemWatcher fs_watcher;
     albert::BackgroundExecutor<std::vector<albert::IndexItem>> indexer;
 };
