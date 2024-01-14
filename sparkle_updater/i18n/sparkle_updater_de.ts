a3a908a352b6155cf4349dea6427ad62e3f5bd04
Manuel Schneider <manuelschneid3r@gmail.com>
2024-01-14 03:41:51 +0100

    [sparkle_updater:2.0]
    * iid:0.23
    * i18n
    * german translation

---
+++ sparkle_updater/i18n/sparkle_updater_de.ts
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE">
+<context>
+    <name>ConfigWidget</name>
+    <message>
+        <location filename="../src/configwidget.ui" line="9"/>
+        <source>Automatically check for updates:</source>
+        <translation>Automatisch nach Updates suchen:</translation>
+    </message>
+    <message>
+        <location filename="../src/configwidget.ui" line="19"/>
+        <source>Automatically download updates:</source>
+        <translation>Updates automatisch herunterladen:</translation>
+    </message>
+    <message>
+        <location filename="../src/configwidget.ui" line="33"/>
+        <source>Last update check:</source>
+        <translation>Letzte Überprüfung auf Updates:</translation>
+    </message>
+    <message>
+        <location filename="../src/configwidget.ui" line="53"/>
+        <source>Check for updates</source>
+        <translation>Nach Updates suchen</translation>
+    </message>
+    <message>
+        <location filename="../src/configwidget.ui" line="73"/>
+        <source>None</source>
+        <translation>Keine</translation>
+    </message>
+</context>
+</TS>
