d9178b8629745f5c0e72e64b2459e2a88f366523
Manuel Schneider <manuelschneid3r@gmail.com>
2024-03-06 22:43:37 +0100

    [debug]

--- debug/src/plugin.cpp
+++ debug/src/plugin.cpp
@@ -1,13 +1,17 @@
 // Copyright (c) 2023 Manuel Schneider
 
-#include "albert/albert.h"
-#include "albert/extension/queryhandler/standarditem.h"
-#include "albert/logging.h"
 #include "plugin.h"
+#include <albert/logging.h>
+#include <albert/notification.h>
+#include <albert/standarditem.h>
+#include <albert/util.h>
 #include <chrono>
 #include <thread>
 ALBERT_LOGGING_CATEGORY("debug")
+using namespace albert;
 using namespace std;
+
+static auto icon = {QStringLiteral("qsp:SP_MessageBoxWarning")};
 
 Plugin::Plugin() { DEBG << "'Debug' created."; }
 
@@ -17,24 +21,50 @@ QString Plugin::synopsis() const { return "debug-debug-debug-debug"; }
 
 bool Plugin::allowTriggerRemap() const { return false; }
 
-void Plugin::handleTriggerQuery(TriggerQuery *query) const
+void Plugin::handleTriggerQuery(albert::Query *query)
 {
-    for(int i = 0; query->isValid() && i < 7; ++i) {
-        for (int nap = 0; nap < 300; ++nap){
-            if (!query->isValid())
-                return;
-            this_thread::sleep_for(chrono::milliseconds(10));
+    if (query->string() == QStringLiteral("busy"))
+    {
+        for(int i = 0; query->isValid() && i < 3; ++i)
+        {
+            for (int nap = 0; nap < 100; ++nap){
+                if (!query->isValid())
+                    return;
+                this_thread::sleep_for(chrono::milliseconds(10));
+            }
+            query->add(
+                albert::StandardItem::make(
+                    {}, QString("Item #%1").arg(i), QString("Wow, Item #%1").arg(i), icon,
+                    {
+                        {"Debug","Open website", [](){ albert::openWebsite(); }}
+                    }
+                )
+            );
         }
+        return;
+    }
+
+    if (QStringLiteral("notification").startsWith(query->string()))
+    {
         query->add(albert::StandardItem::make(
-            QString(),
-            QString("Item #%1").arg(i),
-            QString("Wow, Item #%1").arg(i),
-            QString("%1Item #%2").arg(query->trigger()).arg(i),
-            {"qsp:SP_MessageBoxWarning"},
+            {}, "Memleaking notification", "Leaks memory", icon,
             {
-                    {"Debug","Open website", [](){ albert::openWebsite(); }},
-                    {"Debug","Open settings", [](){ albert::showSettings(); }}
+                {
+                    "Debug", "Open website",
+                    [](){
+                        auto *n = new Notification;
+                        n->setTitle("Memory leak");
+                        n->setText("Ok.");
+                        n->send();
+                    }
+                }
             }
         ));
+    }
+
+    if (QStringLiteral("busy").startsWith(query->string()))
+    {
+        query->add(albert::StandardItem::make(
+                   {}, "busy", "Test delayed queries", "debug busy", icon, {}));
     }
 }
--- debug/src/plugin.h
+++ debug/src/plugin.h
@@ -1,17 +1,17 @@
 // Copyright (c) 2022 Manuel Schneider
 #pragma once
-#include "albert/plugin.h"
-#include "albert/extension/queryhandler/triggerqueryhandler.h"
+#include <albert/extensionplugin.h>
+#include <albert/triggerqueryhandler.h>
 
-class Plugin : public albert::plugin::ExtensionPlugin,
+class Plugin : public albert::ExtensionPlugin,
                public albert::TriggerQueryHandler
 {
-    Q_OBJECT ALBERT_PLUGIN
+    ALBERT_PLUGIN
 public:
     Plugin();
     ~Plugin() override;
 
     bool allowTriggerRemap() const override;
     QString synopsis() const override;
-    void handleTriggerQuery(TriggerQuery*) const override;
+    void handleTriggerQuery(albert::Query*) override;
 };
