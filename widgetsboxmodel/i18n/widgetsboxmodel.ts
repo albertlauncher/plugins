e288062e6a8aff4678b4a38e5e46cf336a3a97ad
Manuel Schneider <manuelschneid3r@gmail.com>
2024-08-22 11:22:11 +0200

    [widgetsboxmodel] Do not install archived themes

--- widgetsboxmodel/i18n/widgetsboxmodel.ts
+++ widgetsboxmodel/i18n/widgetsboxmodel.ts
@@ -4,132 +4,106 @@
 <context>
     <name>ConfigWidget</name>
     <message>
-        <location filename="../src/configwidget.ui" line="14"/>
         <source>Frontend settings</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/configwidget.ui" line="20"/>
         <source>Enable history search</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/configwidget.ui" line="30"/>
         <source>Use the current input text to filter the input history.</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/configwidget.ui" line="40"/>
         <source>Clear input line on hide</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/configwidget.ui" line="50"/>
         <source>Clear the input line when the window is hidden.</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/configwidget.ui" line="66"/>
         <source>Always on top</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/configwidget.ui" line="76"/>
         <source>Let Albert stay on top of all top level windows.</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/configwidget.ui" line="89"/>
         <source>Quit on close</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/configwidget.ui" line="106"/>
         <source>Hide on focus out</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/configwidget.ui" line="116"/>
         <source>Hide Albert if the window lost focus.</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/configwidget.ui" line="129"/>
         <source>Show centered</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/configwidget.ui" line="139"/>
         <source>Position the window centered on the screen. See also &quot;Center on active screen&quot;.</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/configwidget.ui" line="146"/>
         <source>Center on active screen</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/configwidget.ui" line="153"/>
         <source>Show the window on the active screen (the one containing the mouse cursor) rather than the primary one. This option applies when &quot;Show centered&quot; is activated, but also if the window has to be centered on another screen (e.g. because a display has been disconnected).</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/configwidget.ui" line="169"/>
         <source>Light theme</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/configwidget.ui" line="185"/>
         <source>Set the color theme used in light mode.</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/configwidget.ui" line="192"/>
         <source>Dark theme</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/configwidget.ui" line="205"/>
         <source>Set the color theme used in dark mode.</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/configwidget.ui" line="224"/>
         <source>&lt;span style=&quot; font-size:9pt; color:#808080;&quot;&gt;See the &lt;a href=&quot;https://github.com/albertlauncher/plugins/blob/master/widgetsboxmodel/README.md&quot;&gt;readme&lt;/a&gt; for instructions on how to create your own theme. If you created a nice theme please &lt;a href=&quot;https://github.com/orgs/albertlauncher/discussions/1147&quot;&gt;share it&lt;/a&gt;.&lt;/span&gt;</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/configwidget.ui" line="246"/>
         <source>Displayed items</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/configwidget.ui" line="275"/>
         <source>Display system shadow</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/configwidget.ui" line="285"/>
         <source>Tell the system to draw a shadow around the window. Note that on some systems this may look pretty ugly. Use the client shadow in this case.</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/configwidget.ui" line="295"/>
         <source>Display client shadow</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/configwidget.ui" line="305"/>
         <source>Draw a shadow on the client side.</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/configwidget.ui" line="318"/>
         <source>Display scrollbar</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/configwidget.ui" line="328"/>
         <source>Displays a scrollbar in the results list.</source>
         <translation></translation>
     </message>
@@ -137,37 +111,30 @@
 <context>
     <name>Window</name>
     <message>
-        <location filename="../src/themesqueryhandler.cpp" line="20"/>
         <source>Switch themes</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/themesqueryhandler.cpp" line="39"/>
         <source>Apply theme</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/themesqueryhandler.cpp" line="43"/>
         <source>Use in light mode</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/themesqueryhandler.cpp" line="47"/>
         <source>Use in dark mode</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/themesqueryhandler.cpp" line="51"/>
         <source>Open theme file</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="226"/>
         <source>Theme &apos;%1&apos; does not exist. Check your config!</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="647"/>
         <source>The theme file could not be opened</source>
         <translation></translation>
     </message>
--- widgetsboxmodel/i18n/widgetsboxmodel_de.ts
+++ widgetsboxmodel/i18n/widgetsboxmodel_de.ts
@@ -4,132 +4,106 @@
 <context>
     <name>ConfigWidget</name>
     <message>
-        <location filename="../src/configwidget.ui" line="14"/>
         <source>Frontend settings</source>
         <translation>Frontend-Einstellungen</translation>
     </message>
     <message>
-        <location filename="../src/configwidget.ui" line="20"/>
         <source>Enable history search</source>
         <translation>Verlaufssuche aktivieren</translation>
     </message>
     <message>
-        <location filename="../src/configwidget.ui" line="30"/>
         <source>Use the current input text to filter the input history.</source>
         <translation>Verwenden Sie den aktuellen Eingabetext, um den Verlauf zu filtern.</translation>
     </message>
     <message>
-        <location filename="../src/configwidget.ui" line="40"/>
         <source>Clear input line on hide</source>
         <translation>Eingabezeile beim Ausblenden leeren</translation>
     </message>
     <message>
-        <location filename="../src/configwidget.ui" line="50"/>
         <source>Clear the input line when the window is hidden.</source>
         <translation>Leeren Sie die Eingabezeile, wenn das Fenster ausgeblendet wird.</translation>
     </message>
     <message>
-        <location filename="../src/configwidget.ui" line="66"/>
         <source>Always on top</source>
         <translation>Immer im Vordergrund</translation>
     </message>
     <message>
-        <location filename="../src/configwidget.ui" line="76"/>
         <source>Let Albert stay on top of all top level windows.</source>
         <translation>Lassen Sie Albert über allen Top-Level-Fenstern bleiben.</translation>
     </message>
     <message>
-        <location filename="../src/configwidget.ui" line="89"/>
         <source>Quit on close</source>
         <translation>Beim Schließen beenden</translation>
     </message>
     <message>
-        <location filename="../src/configwidget.ui" line="106"/>
         <source>Hide on focus out</source>
         <translation>Beim Fokusverlust verbergen</translation>
     </message>
     <message>
-        <location filename="../src/configwidget.ui" line="116"/>
         <source>Hide Albert if the window lost focus.</source>
         <translation>Albert verbergen, wenn das Fenster den Fokus verliert.</translation>
     </message>
     <message>
-        <location filename="../src/configwidget.ui" line="129"/>
         <source>Show centered</source>
         <translation>Zentriert anzeigen</translation>
     </message>
     <message>
-        <location filename="../src/configwidget.ui" line="139"/>
         <source>Position the window centered on the screen. See also &quot;Center on active screen&quot;.</source>
         <translation>Fenster zentriert auf dem Bildschirm positionieren. Siehe auch &quot;Auf dem aktiven Bildschirm zentrieren&quot;.</translation>
     </message>
     <message>
-        <location filename="../src/configwidget.ui" line="146"/>
         <source>Center on active screen</source>
         <translation>Auf dem aktiven Bildschirm zentrieren</translation>
     </message>
     <message>
-        <location filename="../src/configwidget.ui" line="153"/>
         <source>Show the window on the active screen (the one containing the mouse cursor) rather than the primary one. This option applies when &quot;Show centered&quot; is activated, but also if the window has to be centered on another screen (e.g. because a display has been disconnected).</source>
         <translation>Fenster auf dem aktiven Bildschirm (dem mit dem Mauszeiger) anzeigen, anstatt auf dem primären Bildschirm. Diese Option gilt, wenn &quot;Zentriert anzeigen&quot; aktiviert ist, aber auch, wenn das Fenster auf einem anderen Bildschirm zentriert werden muss (z.B. weil ein Bildschirm getrennt wurde).</translation>
     </message>
     <message>
-        <location filename="../src/configwidget.ui" line="169"/>
         <source>Light theme</source>
         <translation>Helles Thema</translation>
     </message>
     <message>
-        <location filename="../src/configwidget.ui" line="185"/>
         <source>Set the color theme used in light mode.</source>
         <translation>Das Farbthema des hellen Modus festlegen.</translation>
     </message>
     <message>
-        <location filename="../src/configwidget.ui" line="224"/>
         <source>&lt;span style=&quot; font-size:9pt; color:#808080;&quot;&gt;See the &lt;a href=&quot;https://github.com/albertlauncher/plugins/blob/master/widgetsboxmodel/README.md&quot;&gt;readme&lt;/a&gt; for instructions on how to create your own theme. If you created a nice theme please &lt;a href=&quot;https://github.com/orgs/albertlauncher/discussions/1147&quot;&gt;share it&lt;/a&gt;.&lt;/span&gt;</source>
         <translation>&lt;span style=&quot; font-size:9pt; color:#808080;&quot;&gt;Siehe das &lt;a href=&quot;https://github.com/albertlauncher/plugins/blob/master/widgetsboxmodel/README.md&quot;&gt;Readme&lt;/a&gt; für Anleitungen zur Erstellung Ihres eigenen Themas. Wenn Sie ein schönes Thema erstellt haben, teilen Sie es bitte &lt;a href=&quot;https://github.com/orgs/albertlauncher/discussions/1147&quot;&gt;hier&lt;/a&gt;.&lt;/span&gt;</translation>
     </message>
     <message>
-        <location filename="../src/configwidget.ui" line="246"/>
         <source>Displayed items</source>
         <translation>Angezeigte Elemente</translation>
     </message>
     <message>
-        <location filename="../src/configwidget.ui" line="275"/>
         <source>Display system shadow</source>
         <translation>Systemschatten anzeigen</translation>
     </message>
     <message>
-        <location filename="../src/configwidget.ui" line="285"/>
         <source>Tell the system to draw a shadow around the window. Note that on some systems this may look pretty ugly. Use the client shadow in this case.</source>
         <translation>Teilen Sie dem System mit, einen Schatten um das Fenster zu zeichnen. Beachten Sie, dass dies auf einigen Systemen ziemlich hässlich aussehen kann. Verwenden Sie in diesem Fall den Clientschatten.</translation>
     </message>
     <message>
-        <location filename="../src/configwidget.ui" line="295"/>
         <source>Display client shadow</source>
         <translation>Clientschatten anzeigen</translation>
     </message>
     <message>
-        <location filename="../src/configwidget.ui" line="305"/>
         <source>Draw a shadow on the client side.</source>
         <translation>Zeichnen Sie einen Schatten auf der Client-Seite.</translation>
     </message>
     <message>
-        <location filename="../src/configwidget.ui" line="318"/>
         <source>Display scrollbar</source>
         <translation>Scrollbalken anzeigen</translation>
     </message>
     <message>
-        <location filename="../src/configwidget.ui" line="328"/>
         <source>Displays a scrollbar in the results list.</source>
         <translation>Zeigt einen Scrollbalken in der Ergebnisliste an.</translation>
     </message>
     <message>
-        <location filename="../src/configwidget.ui" line="205"/>
         <source>Set the color theme used in dark mode.</source>
         <translation>Das Farbthema des dunklen Modus festlegen.</translation>
     </message>
     <message>
-        <location filename="../src/configwidget.ui" line="192"/>
         <source>Dark theme</source>
         <translation>Dunkles Thema</translation>
     </message>
@@ -137,37 +111,30 @@
 <context>
     <name>Window</name>
     <message>
-        <location filename="../src/themesqueryhandler.cpp" line="20"/>
         <source>Switch themes</source>
         <translation>Wechsle Themen</translation>
     </message>
     <message>
-        <location filename="../src/themesqueryhandler.cpp" line="39"/>
         <source>Apply theme</source>
         <translation>Thema anwenden</translation>
     </message>
     <message>
-        <location filename="../src/themesqueryhandler.cpp" line="43"/>
         <source>Use in light mode</source>
         <translation>Im hellen Modus verwenden</translation>
     </message>
     <message>
-        <location filename="../src/themesqueryhandler.cpp" line="47"/>
         <source>Use in dark mode</source>
         <translation>Im dunklen Modus verwenden</translation>
     </message>
     <message>
-        <location filename="../src/themesqueryhandler.cpp" line="51"/>
         <source>Open theme file</source>
         <translation>Themendatei öffnen</translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="226"/>
         <source>Theme &apos;%1&apos; does not exist. Check your config!</source>
         <translation>Thema &apos;%1&apos; existiert nicht. Prüfen Sie ihre Konfiguration!</translation>
     </message>
     <message>
-        <location filename="../src/window.cpp" line="647"/>
         <source>The theme file could not be opened</source>
         <translation>Die Themendatei konnte nicht geöffnet werden</translation>
     </message>
