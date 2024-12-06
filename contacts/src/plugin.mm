e8ce68428a493a510e616ae7a026188e6bd38f99
Manuel Schneider <manuelschneid3r@gmail.com>
2024-12-06 12:30:13 +0100

    [contacts] Revert implicit ARC

--- contacts/src/plugin.mm
+++ contacts/src/plugin.mm
@@ -9,6 +9,9 @@
 ALBERT_LOGGING_CATEGORY("contacts")
 using namespace albert;
 using namespace std;
+#if  ! __has_feature(objc_arc)
+#error This file must be compiled with ARC.
+#endif
 
 
 class Plugin::Private
